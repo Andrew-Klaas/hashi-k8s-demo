bb1e9958cdc7d4ea0d7f988bf362a0c2c02ef8d8
Andrew Klaas <aklaas@hashicorp.com>
2020-02-20 15:30:49 -0600

    updatig tf code

--- main.tf
+++ main.tf
@@ -6,25 +6,25 @@ provider "google" {
   //Use the credentials file or environment variables
   //https://www.terraform.io/docs/providers/google/provider_reference.html
   //credentials = "${file("gcp-creds.json")}"
-  project     = "${var.gcp_project}"
-  region      = "${var.gcp_region}"
+  project     = var.gcp_project
+  region      = var.gcp_region
 }
 
 resource "google_container_cluster" "k8sexample" {
-  name               = "${var.cluster_name}"
+  name               = var.cluster_name
   description        = "k8s demo cluster"
-  location               = "${var.gcp_zone}"
-  initial_node_count = "${var.initial_node_count}"
+  location               = var.gcp_zone
+  initial_node_count = var.initial_node_count
   enable_legacy_abac = "true"
 
   master_auth {
-    username = "${var.master_username}"
-    password = "${var.master_password}"
+    username = var.master_username
+    password = var.master_password
   }
 
   node_config {
-    machine_type = "${var.node_machine_type}"
-    disk_size_gb = "${var.node_disk_size}"
+    machine_type = var.node_machine_type
+    disk_size_gb = var.node_disk_size
     oauth_scopes = [
       "https://www.googleapis.com/auth/compute",
       "https://www.googleapis.com/auth/devstorage.read_only",
